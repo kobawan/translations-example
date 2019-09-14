ecce785cf2991d8e48b18ac3227948243f0cc543
Sara Nordmyr da Cunha <saranordmyr@hotmail.com>
2019-09-14 19:06:58 +0200

    Updates

---
+++ precache-manifest.d5ada545334cd20233c7219cd81bd38b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7df302c9a21362a5ad98c7153d2cc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30bad62406cec92adce9",
+    "url": "/static/css/main.69112d1f.chunk.css"
+  },
+  {
+    "revision": "51161ddb4cc6534f362c",
+    "url": "/static/js/2.2b12b7cb.chunk.js"
+  },
+  {
+    "revision": "30bad62406cec92adce9",
+    "url": "/static/js/main.3d52dacc.chunk.js"
+  },
+  {
+    "revision": "9930a323f91a755fa490",
+    "url": "/static/js/runtime~main.30173bed.js"
+  }
+]);
